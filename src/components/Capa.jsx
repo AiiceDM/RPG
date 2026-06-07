3211cffbb32e8fef0f42f7c492c030e8eb85e61f
Aiice <iagotorres16@gmail.com>
2026-06-07 19:31:27 -0300

    Definição do Nome do RPG

--- src/components/Capa.jsx
+++ src/components/Capa.jsx
@@ -4,7 +4,7 @@ export default function Capa() {
   return (
     <main className="pagina-site capa-site">
       <div className="conteudo-capa">
-        <h1>Nome do Seu RPG</h1>
+        <h1>Tyria</h1>
         <p>A aventura começa aqui. Uma frase de efeito épica para chamar os jogadores.</p>
         <button className="botao-acao">Iniciar Jornada</button>
       </div>
